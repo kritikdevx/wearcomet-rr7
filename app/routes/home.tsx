3377e483d38eef846331ab7f01e532ec4f84b6ba
kritikdevx <kritik.jiyaviya@devxconsultancy.com>
2024-12-14 18:48:34 +0530

    remove suspense

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,6 @@ import { shopifyClient } from "~/libs/shopify";
 import type { Route } from "./+types/home";
 import type { Product } from "@shopify/hydrogen-react/storefront-api-types";
 import { flattenConnection, Image } from "@shopify/hydrogen-react";
-import { Suspense } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -82,21 +81,19 @@ export default function Home({
   loaderData: { products },
 }: Route.ComponentProps) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {products.map((product) => (
-          <div key={product.id}>
-            <Image
-              src={product.images.edges[0].node.url}
-              alt={product.images.edges[0].node.altText || product.title}
-              loading="eager"
-              sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33.333vw, (min-width: 640px) 50vw, 100vw"
-            />
-            <p>{product.title}</p>
-            <p>{product.priceRange.minVariantPrice.amount}</p>
-          </div>
-        ))}
-      </div>
-    </Suspense>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {products.map((product) => (
+        <div key={product.id}>
+          <Image
+            src={product.images.edges[0].node.url}
+            alt={product.images.edges[0].node.altText || product.title}
+            loading="eager"
+            sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33.333vw, (min-width: 640px) 50vw, 100vw"
+          />
+          <p>{product.title}</p>
+          <p>{product.priceRange.minVariantPrice.amount}</p>
+        </div>
+      ))}
+    </div>
   );
 }
